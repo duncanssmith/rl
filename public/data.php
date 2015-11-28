2efd9aab192d1e8efda85bb7d298cd95c7a89dcb
Duncan Smith <duncanssmith@gmail.com>
2015-11-28 18:32:49 +0000

    Initial

---
+++ public/data.php
@@ -0,0 +1,17 @@
+<?php
+
+$images=Array(
+	'DSC01843.jpg',
+	'DSC01969.jpg',
+	'DSC01971.jpg',
+	'DSC01976.jpg',
+	'DSC01980.jpg',
+	'DSC02030.jpg',
+	'DSC90001.jpg',
+	'DSC90002.jpg',
+	'DSC90003.jpg',
+	'DSC9Portland4.jpg',
+	'DSC9Portland6.jpg'
+);
+
+?>
