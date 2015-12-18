078aea71153cdbad41c0615b64126c7f023e00fb
Duncan Smith <duncanssmith@gmail.com>
2015-12-18 10:02:57 +0000

    Format HTML

--- public/terms.php
+++ public/terms.php
@@ -1,85 +1,81 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <?php include 'head.php';?>
-<body data-spy="scroll" data-target="#rlo-navbar" data-offset="90">
-<?php include 'nav.php';?>
-<div id="content">
-    <section id="section-terms" class="section photo">
-        <div class="container">
-            <div class="row">
-                <h1 class="title">Booking Terms and Conditions</h1>
-            </div>
-            <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-10">
-	                 <p class="terms">Please read carefully.  <a class="btn btn-default" href="media/images/rl_terms_conditions.pdf" target="_blank">You can download a PDF copy here.</a> </p>
+    <body data-spy="scroll" data-target="#rlo-navbar" data-offset="90">
+    <?php include 'nav.php';?>
+        <div id="content">
+            <section id="section-terms" class="section photo">
+                <div class="container">
+                    <div class="row">
+                        <h1 class="title">Booking Terms and Conditions</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-10">
+                            <p class="terms">Please read carefully.  <a class="btn btn-default" href="media/images/rl_terms_conditions.pdf" target="_blank">You can download a PDF copy here.</a> </p>
+                            <ol>
+                                <li>
+                                The property known as Ranters Lodge. The Owner known as Judith Frost. The Renter and their party shall henceforth in this document be known as the Client.
+                                </li>
+                                <li>
+                                To reserve the property the Client should e-mail, phone or text the Owner and ask about availability. Hopefully a suitable rental period is agreed.
+                                </li>
+                                <li>
+                                The Client should send the 20% non returnable deposit either by bank transfer or cheque to the owner (Unless otherwise agreed with the Owner). When the deposit has been received by the owner the property will be booked to the Client and the remainder of the monies are due 4 weeks before the start of the rental. If the booking is made after this period the whole of the monies are due at the time of booking (Unless otherwise agreed with the Owner).
+                                </li>
+                                <li>
+                                If the Client has to cancel their booking, the Owner will make every effort to re-let the property. Money will be returned to them -10%, if another Client is found. If that is not the case then the Client will be liable for the entire cost of the Holiday. The Owner suggests that the Client takes out holiday insurance to cover any loss they may incur if they cancel the booking. If for any reason the monies have not been received by the Owner, prior to the start of the holiday and the Client has to cancel their booking the Owner would still expect the whole sum owed to be paid unless another client was found and then it would be 10% of the sum owed.
+                                </li>
+                                <li>
+                                The maximum number of people to reside in the property shall not exceed 3 unless the Owner has given permission in writing.
+                                </li>
+                                <li>
+                                The rental period shall commence at 12 noon on the first day and finish at 12 noon on the day of departure.
+                                </li>
+                                <li>
+                                The property is a No Smoking Property. The Client agrees that no one in his or her party will smoke inside the property. This is a condition of the insurance
+                                </li>
+                                <li>
+                                The Owner will accept one well-behaved dog as long as the Owner is advised in advance. Extra care must be taken to clean when leaving the property, The dog must be kept off the beds and the soft furnishings and any dog mess removed immediately from the shared yard. The dog must not be left alone in the property if likely to be stressed.
+                                </li>
+                                <li>
+                                The Client agrees to take good care of the property and to leave the property in a clean and tidy condition at the end of the rental period. There may not be a cleaner between every rental (This is reflected in the price charged) so it is important that the property is left as the Client would expect to find it.
+                                </li>
+                                <li>
+                                The Client agrees to notify the owner of any defects in the property and the Owner will make every effort to rectify the situation as soon as possible by either repair or replacement which ever is reasonably possible.
+                                </li>
+                                <li>
+                                Complaints must be made immediately to the owner by the Client, so the owner has the opportunity to rectify the situation. No refund will be given if the Client fails to notify the Owner of their complaint before the end of their stay.
+                                </li>
+                                <li>
+                                The Client agrees to notify the Owner of any breakages and be liable for appropriate payment, through discussion with the Owner.
+                                </li>
+                                <li>
+                                The owner shall not be liable to the client:
+                                    <ul>
+                                        <li>
+                                        a) For any temporary stoppage in relation to public or private services to the property nor in respect to any failure of machinery, appliances, phone, or internet in the property.
+                                        </li>
+                                        <li>
+                                        b) For any loss, damage or injury, which is the result of adverse weather conditions, riot, war, terrorism, industrial disputes or any other matter beyond the control of the owner.
+                                        </li>
+                                        <li>
+                                        c) For any loss damage or inconvenience to the Client caused to or suffered by the client in respect of the property being destroyed or substantially damaged before the start of the rental. The Owner would return all monies paid in advance within 7 days of notification of this fact to the Client.
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                Under no circumstances shall the Owner's liability to the Client exceed the amount paid to the Owner for the rental period
+                                </li>
+                                <li>
+                                In the exceptional event of the property being on "flood warning" and "severe flood warning" (as described and implemented by the Environment Agency) the Client will have the right to have their monies returned if they decide to leave.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
 
-
-	<ol>
-	<li>
-	The property known as Ranters Lodge. The Owner known as Judith Frost. The Renter and their party shall henceforth in this document be known as the Client.
-	</li>
-	<li>
-	To reserve the property the Client should e-mail, phone or text the Owner and ask about availability. Hopefully a suitable rental period is agreed.
-	</li>
-	<li>
-	The Client should send the 20% non returnable deposit either by bank transfer or cheque to the owner (Unless otherwise agreed with the Owner). When the deposit has been received by the owner the property will be booked to the Client and the remainder of the monies are due 4 weeks before the start of the rental. If the booking is made after this period the whole of the monies are due at the time of booking (Unless otherwise agreed with the Owner).
-	</li>
-	<li>
-	If the Client has to cancel their booking, the Owner will make every effort to re-let the property. Money will be returned to them -10%, if another Client is found. If that is not the case then the Client will be liable for the entire cost of the Holiday. The Owner suggests that the Client takes out holiday insurance to cover any loss they may incur if they cancel the booking. If for any reason the monies have not been received by the Owner, prior to the start of the holiday and the Client has to cancel their booking the Owner would still expect the whole sum owed to be paid unless another client was found and then it would be 10% of the sum owed.
-	</li>
-	<li>
-	The maximum number of people to reside in the property shall not exceed 3 unless the Owner has given permission in writing.
-	</li>
-	<li>
-	The rental period shall commence at 12 noon on the first day and finish at 12 noon on the day of departure.
-	</li>
-	<li>
-	The property is a No Smoking Property. The Client agrees that no one in his or her party will smoke inside the property. This is a condition of the insurance
-	</li>
-	<li>
-	The Owner will accept one well-behaved dog as long as the Owner is advised in advance. Extra care must be taken to clean when leaving the property, The dog must be kept off the beds and the soft furnishings and any dog mess removed immediately from the shared yard. The dog must not be left alone in the property if likely to be stressed.
-	</li>
-	<li>
-	The Client agrees to take good care of the property and to leave the property in a clean and tidy condition at the end of the rental period. There may not be a cleaner between every rental (This is reflected in the price charged) so it is important that the property is left as the Client would expect to find it.
-	</li>
-	<li>
-	The Client agrees to notify the owner of any defects in the property and the Owner will make every effort to rectify the situation as soon as possible by either repair or replacement which ever is reasonably possible.
-	</li>
-	<li>
-	Complaints must be made immediately to the owner by the Client, so the owner has the opportunity to rectify the situation. No refund will be given if the Client fails to notify the Owner of their complaint before the end of their stay.
-	</li>
-	<li>
-	The Client agrees to notify the Owner of any breakages and be liable for appropriate payment, through discussion with the Owner.
-	</li>
-	<li>
-	The owner shall not be liable to the client:
-	<ul>
-	<li>
-	a) For any temporary stoppage in relation to public or private services to the property nor in respect to any failure of machinery, appliances, phone, or internet in the property.
-	</li>
-	<li>
-	b) For any loss, damage or injury, which is the result of adverse weather conditions, riot, war, terrorism, industrial disputes or any other matter beyond the control of the owner.
-	</li>
-	<li>
-	c) For any loss damage or inconvenience to the Client caused to or suffered by the client in respect of the property being destroyed or substantially damaged before the start of the rental. The Owner would return all monies paid in advance within 7 days of notification of this fact to the Client.
-	</li>
-
-	</ul>
-
-	</li>
-
-	<li>
-	Under no circumstances shall the Owner's liability to the Client exceed the amount paid to the Owner for the rental period
-	</li>
-	<li>
-	In the exceptional event of the property being on "flood warning" and "severe flood warning" (as described and implemented by the Environment Agency) the Client will have the right to have their monies returned if they decide to leave.
-	</li>
-	</ol>
-	</div>
-	</div>
-	</div>
-</div>
-
-</body>
+    </body>
 </html>
