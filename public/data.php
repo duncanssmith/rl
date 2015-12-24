7d946cba35af35b94df0d7c5d6b35780d8a7f7c4
Duncan Smith <duncanssmith@gmail.com>
2015-12-24 10:14:42 +0000

    Legibility improvements

--- public/data.php
+++ public/data.php
@@ -1,19 +1,18 @@
 <?php
 
 $images = Array(
-	'DSC01843.jpg',
-	'DSC01969.jpg',
-	'DSC01971.jpg',
-	'DSC01976.jpg',
-	'DSC01980.jpg',
-	'DSC02030.jpg',
-	'DSC90001.jpg',
-	'DSC90002.jpg',
-	'DSC90003.jpg',
-	'DSC9Portland4.jpg',
+    'DSC01843.jpg',
+    'DSC01969.jpg',
+    'DSC01971.jpg',
+    'DSC01976.jpg',
+    'DSC01980.jpg',
+    'DSC02030.jpg',
+    'DSC90001.jpg',
+    'DSC90002.jpg',
+    'DSC90003.jpg',
+    'DSC9Portland4.jpg',
     'DSC9Portland6.jpg',
     'rl2005_001.jpg',
     'rl2005_002.jpg'
 );
 
-?>
--- public/index.php
+++ public/index.php
@@ -16,20 +16,20 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <h2>Ranters Lodge Artists' Studio/Cottage on Chesil Beach</h2>
-                            <a href="mailto:jcfrost@dialstart.net">Email jcfrost@dialstart.net </a>
-                            <p> Chiswell, Portland, Dorset DT5 1AW<br/>tel +44(0)1305 822 096<br/>mobile +44(0)7887 945 034 </p>
+                            <h2 class="subtitle">Artists' Studio/Cottage</h2>
+                            <h3 class=""><a href="mailto:jcfrost@dialstart.net">Email: jcfrost@dialstart.net </a></h3>
+                            <p class="address">Ranters Lodge<br>Chiswell<br>Portland<br>Dorset DT5 1AW<br>United Kingdom<br>tel +44(0)1305 822 096<br/>mobile +44(0)7887 945 034</p>
                         </div>
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[1]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[1]?>" border="0">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[2]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[2]?>" border="0">
                         </div>
-                        <div class="col-md-6">
-                            <p> Ranters Lodge and the Dead House lie 30 metres from the sea. Formerly a fishing store and a boat house, they have been converted into two self-contained artists' studios with accommodation for 2/3 people each. Normally only Ranters Lodge is rented. </p>
+                        <div class="col-md-6 legible">
+                            <p>Ranters Lodge and the Dead House lie 30 metres from the sea. Formerly a fishing store and a boat house, they have been converted into two self-contained artists' studios with accommodation for 2/3 people each. Normally only Ranters Lodge is rented. </p>
                         </div>
                     </div>
                 </div>
@@ -42,10 +42,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[9]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[9]?>" border="0">
                         </div>
-                        <div class="col-md-6">
-                            <p> Ranters Lodge consists of 2 floors approximately 6 x 7.5 metres. The upper floor has a sleeping platform. All walls are whitewashed, the floors wood and concrete with rugs. On the ground floor there are two large tables and a small kitchen that has a fridge, a mini electric cooker and a small microwave. On the first floor there is a further area with a large, nearly north skylight, a day bed plus the bathroom which has a sitting bath with a shower. The sleeping platform has a "natural Marks and Spencer" 5ft mattress. There is gas central heating throughout. Outside there is a courtyard garden with tables, chairs, and a small barbecue. </p>
+                        <div class="col-md-6 legible">
+                            <p> Ranters Lodge consists of 2 floors approximately 6 x 7.5 metres. The upper floor has a sleeping platform. All walls are whitewashed, the floors wood and concrete with rugs. On the ground floor there are two large tables and a small kitchen that has a fridge, a mini electric cooker and a small microwave. On the first floor there is a further area with a large, nearly-north skylight, a day bed plus the bathroom which has a sitting bath and a walk-in shower. The sleeping platform has a "natural Marks and Spencer" 5ft mattress. There is gas central heating throughout. Outside there is a courtyard garden with tables, chairs, and a small barbecue. </p>
                         </div>
                     </div>
                 </div>
@@ -58,10 +58,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[5]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[5]?>" border="0">
                             <p class="capt">Studio bedroom</p>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 legible">
                           <p>
                           Ranters Lodge is available to rent inclusive of heating at &pound;290 per week, or &pound;44 per night, (minimum three nights), except from July until middle of September, at Christmas and at Easter when rent is &pound;475 per week or &pound;70 per night (minimum of three nights). Longer lets are available. Please phone to discuss a price.
                           </p>
@@ -83,10 +83,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[4]?>" border="0"></a>
+                            <img src="media/images/<?php echo $images[4]?>" border="0">
                             <p class="capt">West Weares</p>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 legible">
                           <p>
                           Ranters Lodge is easily reached by public transport from London Waterloo. There is a twice hourly service taking approximately 2 hours 45 minutes to Weymouth. There is also a train service from Bristol to Weymouth. From Weymouth train station there are taxis or a short walk to the king's statue on the esplanade where there is a very regular bus service to Portland (No. 1 bus to Victoria Square.) Then there is a short walk to Ranters Lodge.</p>
                           <p>
@@ -104,18 +104,18 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[0]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[0]?>" border="0">
                         </div>
-                        <div class="col-md-6">
-                          <p align="left"> Chiswell, part of the Jurassic Coast, a World Heritage Site, is at the eastern end of Chesil beach, an 18 mile bank of stones connecting Portland to the mainland. Portland is well known mainly for its stone, used to rebuild London after the great fire in the 17th Century. It is also the "Isle of Slingers" in Thomas Hardy's novel <u>The Well Beloved</u>. It has many places that have an extraordinary charm and unusual beauty. West Weares, a spectacular walk where quarried stone tumbles into the sea, is a two minute walk from the studio, and Tout Quarry, a site of scientific Interest are just two examples. You can swim on calm days in the corner of Chesil Bay. There is also diving off Chesil Beach and in Portland Harbour, windsurfing and sailing. A yacht club opened in 2004, home of the 2012 Olympics. Close to the studios are the following: a caf&eacute;, an award-winning restaurant, and the Cove House Inn. It is a five minute walk to Fortuneswell where there is a chemist, a Post Office, a supermarket, a caf&eacute;, a fish and chip shop, and an Indian and a Chinese &quot;take-away&quot; restaurant. </p>
+                        <div class="col-md-6 legible">
+                          <p align="left"> Chiswell, part of the Jurassic Coast, a World Heritage Site, is at the eastern end of Chesil beach, an 18 mile bank of stones connecting Portland to the mainland. Portland is well known mainly for its stone, used to rebuild London after the great fire in the 17th Century. It is also the "Isle of Slingers" in Thomas Hardy's novel <u>The Well Beloved</u>. It has many places that have an extraordinary charm and unusual beauty. West Weares, a spectacular walk where quarried stone tumbles into the sea, is a two minute walk from the studio, and Tout Quarry, a site of scientific interest are just two examples. You can swim on calm days in the corner of Chesil Bay. There is also diving off Chesil Beach and in Portland Harbour, windsurfing and sailing. A yacht club opened in 2004 later becoming the home of the sailing events of the 2012 Olympics. Close to the studios are a caf&eacute;, an award-winning restaurant, and the Cove House Inn. It is a five minute walk to Fortuneswell where there is a chemist, a Post Office, a supermarket, a caf&eacute;, a fish and chip shop, and an Indian and a Chinese &quot;take-away&quot; restaurant. </p>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-6 legible">
                           <p> Portland is situated in the centre of the Dorset coastline, 4 miles from Weymouth, a traditional English seaside resort, with a sandy beach. It is within easy reach of the East Dorset Coast, including the Isle of Purbeck, which has a wild, rocky and diverse coastline, and West Dorset which has Chesil Beach, followed by fossil rich cliffs and a spectacular landscape dotted with ancient monuments. </p>
                         </div>
                         <div class="col-md-6">
-                            <a href="index.php#section-pictures"><img src="media/images/<?php echo $images[3]?>" border="0"></a></p>
+                            <img src="media/images/<?php echo $images[3]?>" border="0">
                         </div>
                     </div>
                 </div>
